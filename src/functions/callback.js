60acc5d24aaf4e049d5f395116b01630d1a3dc53
Alex Crocker <hello@crock.dev>
2020-03-04 09:31:11 -0500

    Added sso and callback endpoints

---
+++ src/functions/callback.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+exports.handler = async function(event, context) {
+  const baseUrl = `https://discordapp.com/api`
+
+  const data = {
+    grant_type: 'authorization_code',
+    code: event.queryStringParameters.code,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    scope: process.env.OAUTH_SCOPES,
+    redirect_uri: process.env.REDIRECT_URI,
+  }
+
+  const url = `${baseUrl}/oauth2/token`
+
+  const response = await axios({
+    method: 'post',
+    url,
+    data,
+  })
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({ tokens: response.data }),
+  }
+}
